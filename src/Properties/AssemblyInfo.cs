2281421068e4cda86863eac70d4424131c5d6aa5
mazkdevf <79049205+mazk5145@users.noreply.github.com>
2022-03-19 19:13:06 +0200

    Add files via upload

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConfigSaver")]
+[assembly: AssemblyDescription("Create Encrypted Config for Username and password")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConfigSaver")]
+[assembly: AssemblyCopyright("Copyright © MAZK 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("822d87c4-52fb-4f2c-8b74-54b9c2f97fe2")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
